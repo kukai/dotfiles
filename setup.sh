15cb446a77f0a540fc5014cb1b21a81e5e4c7059
kukai <4723422+kukai@users.noreply.github.com>
2017-12-14 11:00:48 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-DOTFILES=(.tmux.conf .zshrc .config)
+DOTFILES=(.tmux.conf .zshrc .vimrc)
 
 for file in ${DOTFILES[@]}
 do
-  ln -fnsv $HOME/dotfiles/$file $HOME/$file
+  ln -fnsv $PWD/$file $HOME/$file
 done
 
 if [ ! -d ~/.tmux/plugins/tpm ]; then
-  git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 fi
 
 if [ ! -d ~/.zplug ]; then
-  curl -sL zplug.sh/installer | zsh
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 fi
