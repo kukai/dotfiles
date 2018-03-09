aa6df75a3b35b287e0ed26222cb88b3332dddc54
kukai <4723422+kukai@users.noreply.github.com>
2018-03-09 23:55:43 +0900

    use goenv and nodebrew

--- setup.sh
+++ setup.sh
@@ -11,6 +11,17 @@ if [ ! -d ~/.tmux/plugins/tpm ]; then
   git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 fi
 
+if [ ! -d ~/.goenv ]; then
+  goenv install 1.10.0
+  goenv global 1.10.0
+fi
+
+if [ ! -d ~/.nodebrew ]; then
+  curl -L git.io/nodebrew | perl - setup
+  nodebrew install-binary stable
+  nodebrew use stable
+fi
+
 if [ ! -d ~/.zplug ]; then
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 fi
